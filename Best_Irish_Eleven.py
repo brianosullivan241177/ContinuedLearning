24e689c3c65b314c063c18c1b5c49ab0ed8963ed
BrianOSullivan <brian.osullivan@hse.ie>
2022-02-12 16:27:09 +0000

    Saturday

--- Best_Irish_Eleven.py
+++ Best_Irish_Eleven.py
@@ -4,7 +4,7 @@ import pandas as pd
 FifaAll_Players_DF = pd.read_csv('/users/brian/documents/FIFA_Project_Final/Modified/players_21_22.csv',
                            usecols=['sofifa_id','short_name','league_name','club_name','wage_eur', 'overall','age','preferred_foot',
                                     'Season', 'height_cm','nationality_name','potential','international_reputation',
-                                    'goalkeeping_handling'])
+                                    'goalkeeping_handling','defending_sliding_tackle','passing','defending','skill_long_passing','power_strength'])
 print(FifaAll_Players_DF.head()) #top records
 print(FifaAll_Players_DF.dtypes) #dataframe data tyoes
 
@@ -42,3 +42,46 @@ Country1 = Country("Republic of Ireland")
 print(Country1)
 goalkeeper = Country1.iloc[0][1]
 print("The goalkeeper is :", goalkeeper)
+
+Irish_Players_RF_DF['Right_fullback_mean']=Irish_Players_RF_DF['defending_sliding_tackle'] \
+                                  + Irish_Players_RF_DF['passing'] + Irish_Players_RF_DF['defending'] + \
+                                  Irish_Players_RF_DF['skill_long_passing'] + Irish_Players_RF_DF['power_strength']
+Irish_Players_RF_DF
+
+
+def Country(x):
+    def findminmax(col):
+        work = Irish_Players_RF_DF[col].idxmax()
+        work_df = pd.DataFrame(Irish_Players_RF_DF.loc[work])
+        return work_df
+    return Irish_Players_RF_DF[Irish_Players_RF_DF['nationality_name'] == x ][['Right_fullback_mean', 'short_name']]\
+        .sort_values(by=['Right_fullback_mean'],ascending=False)
+
+Country1 = Country("Republic of Ireland")
+print(Country1)
+fullback1=Country1.iloc[0][1]
+print("The fullback one is :", fullback1)
+fullback2=Country1.iloc[1][1]
+print("The fullback two is :", fullback2)
+
+
+# Left full back
+
+Irish_Players_LF_DF['Left_fullback_mean']=Irish_Players_RF_DF['defending_sliding_tackle'] \
+                                  + Irish_Players_RF_DF['passing'] + Irish_Players_RF_DF['defending'] + \
+                                  Irish_Players_RF_DF['skill_long_passing'] + Irish_Players_RF_DF['power_strength']
+Irish_Players_LF_DF
+def Country(x):
+    def findminmax(col):
+        work = Irish_Players_LF_DF[col].idxmax()
+        work_df = pd.DataFrame(Irish_Players_LF_DF.loc[work])
+        return work_df
+    return Irish_Players_LF_DF[Irish_Players_LF_DF['nationality_name'] == x ][['Left_fullback_mean', 'short_name']]\
+        .sort_values(by=['Left_fullback_mean'],ascending=False)
+
+Country1 = Country("Republic of Ireland")
+print(Country1)
+fullback1=Country1.iloc[0][1]
+print("The fullback one is :", fullback1)
+fullback2=Country1.iloc[1][1]
+print("The fullback two is :", fullback2)
