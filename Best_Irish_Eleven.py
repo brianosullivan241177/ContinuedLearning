81f0d830f9928ccb728a7cca244c8168bca6feed
BrianOSullivan <brian.osullivan@hse.ie>
2022-02-16 09:31:43 +0000

    Wednesday

--- Best_Irish_Eleven.py
+++ Best_Irish_Eleven.py
@@ -5,18 +5,18 @@ FifaAll_Players_DF = pd.read_csv('/users/brian/documents/FIFA_Project_Final/Modi
                            usecols=['sofifa_id','short_name','league_name','club_name','wage_eur', 'overall','age','preferred_foot',
                                     'Season', 'height_cm','nationality_name','potential','international_reputation',
                                     'goalkeeping_handling','defending_sliding_tackle','passing','defending','skill_long_passing',
-                                    'power_strength','dribbling','pace','movement_agility'])
+                                    'power_strength','dribbling','pace','movement_agility','movement_reactions','defending_marking_awareness','shooting',
+                                    'attacking_heading_accuracy'])
 print(FifaAll_Players_DF.head()) #top records
 print(FifaAll_Players_DF.dtypes) #dataframe data tyoes
-
+#Left footed Republic of Ireland
 Irish_Players_LF_DF = FifaAll_Players_DF[(FifaAll_Players_DF.nationality_name == 'Republic of Ireland') &
                                       (FifaAll_Players_DF.preferred_foot == 'Left')]
 print(Irish_Players_LF_DF.head())
-
+#Right footed Republic of Ireland
 Irish_Players_RF_DF = FifaAll_Players_DF[(FifaAll_Players_DF.nationality_name == 'Republic of Ireland') &
                                       (FifaAll_Players_DF.preferred_foot == 'Right')]
 print(Irish_Players_RF_DF.head())
-
 
 
 def findminmax(col):
@@ -49,7 +49,7 @@ Irish_Players_RF_DF['Right_fullback_mean']=Irish_Players_RF_DF['defending_slidin
                                   Irish_Players_RF_DF['skill_long_passing'] + Irish_Players_RF_DF['power_strength']
 Irish_Players_RF_DF
 
-
+#Right footed full back Republic of Ireland
 def Country(x):
     def findminmax(col):
         work = Irish_Players_RF_DF[col].idxmax()
@@ -62,14 +62,13 @@ Country1 = Country("Republic of Ireland")
 print(Country1)
 Rightfullback1=Country1.iloc[0][1]
 print("The right fullback one is :", Rightfullback1)
-Rightfullback2=Country1.iloc[1][1]
-print("The right fullback two is :", Rightfullback2)
 
 
+Irish_Players_LF_DF.drop(Irish_Players_LF_DF[Irish_Players_LF_DF['short_name']=='Leftfullback1'].index, inplace = True)
 # Left full back
-Irish_Players_LF_DF['Left_fullback_mean']=Irish_Players_RF_DF['defending_sliding_tackle'] \
-                                  + Irish_Players_RF_DF['passing'] + Irish_Players_RF_DF['defending'] + \
-                                  Irish_Players_RF_DF['skill_long_passing'] + Irish_Players_RF_DF['power_strength']
+Irish_Players_LF_DF['Left_fullback_mean']=Irish_Players_LF_DF['defending_sliding_tackle'] \
+                                  + Irish_Players_LF_DF['passing'] + Irish_Players_LF_DF['defending'] + \
+                                  Irish_Players_LF_DF['skill_long_passing'] + Irish_Players_LF_DF['power_strength']
 Irish_Players_LF_DF
 def Country(x):
     def findminmax(col):
@@ -83,15 +82,37 @@ Country1 = Country("Republic of Ireland")
 print(Country1)
 Leftfullback1=Country1.iloc[0][1]
 print("The Left fullback one is :", Leftfullback1)
-Leftfullback2=Country1.iloc[1][1]
-print("The Left fullback two is :", Leftfullback2)
 
-print(Irish_Players_RF_DF.head())
+print("*********************************************")
+print("***********************Left Centre Back **********************")
+# Left centre back
 
-# Excludes the right full back from being picked in midfield
-Excludealreadypicked = Irish_Players_RF_DF.drop(Irish_Players_RF_DF[Irish_Players_RF_DF['short_name']==Rightfullback2].index, inplace = True)
+Irish_Players_LF_DF['Left_centreback_mean']=Irish_Players_LF_DF['defending_sliding_tackle'] \
+                                  + Irish_Players_LF_DF['passing'] + Irish_Players_LF_DF['defending'] + \
+                                  Irish_Players_LF_DF['skill_long_passing'] + Irish_Players_LF_DF['power_strength']
 
-print(Irish_Players_RF_DF.head())
+Irish_Players_LF_DF.drop(Irish_Players_LF_DF[Irish_Players_LF_DF['short_name']=='Leftfullback1'].index, inplace = True)
+
+def Country(x):
+    def findminmax(col):
+        work = Irish_Players_LF_DF[col].idxmax()
+        work_df = pd.DataFrame(Irish_Players_LF_DF.loc[work])
+        return work_df
+    return Irish_Players_LF_DF[Irish_Players_LF_DF['nationality_name'] == x ][['Left_centreback_mean', 'short_name']]\
+        .sort_values(by=['Left_centreback_mean'],ascending=False)
+
+Country1 = Country("Republic of Ireland")
+print(Country1)
+Leftcentreback1=Country1.iloc[0][1]
+print("The Left centre back is :", Leftcentreback1)
 
 
 
+print("***********************Centre Back **********************")
+
+
+print("*********************************************")
+
+print("The goalkeeper is :", goalkeeper)
+print("The right fullback one is :", Rightfullback1)
+print("The Left fullback one is :", Leftfullback1)
